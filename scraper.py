85604d668a7f468e9f43ead229a9c214f7d5ebb8
Zyndro <lis.mateusz@ymail.com>
2019-12-14 13:55:22 +0100

    network error handling

--- scraper.py
+++ scraper.py
@@ -9,14 +9,16 @@ def netto(brutto):
         table=[]
         url = "https://zarobki.pracuj.pl/kalkulator-wynagrodzen?Salary="+brutto+"&SalaryType=1"
         r = c.get(url)
-        soup = BeautifulSoup(r.content, 'html5lib')
-        for table_data in soup.find_all('span', {'class': 'value smaller'}):
-            table.append(table_data)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'html5lib')
+            for table_data in soup.find_all('span', {'class': 'value smaller'}):
+                table.append(table_data)
 
-        netto=table[0]
-        output = re.findall('\d+', str(netto))
-        output1 = "".join(output)
-
+            netto=table[0]
+            output = re.findall('\d+', str(netto))
+            output1 = "".join(output)
+        else:
+            raise requests.HTTPError
 
     return(output1)
 
