8bff25f318b098c244e9550666564841dcb56a3c
Zyndro <lis.mateusz@ymail.com>
2019-12-30 04:06:11 +0100

    Wynagrodzenia.pl scraper

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-
+#get request
 def netto(brutto,rodzaj):
     umowa=rodzaj
     brutto=str(brutto)
@@ -32,7 +32,7 @@ def netto(brutto,rodzaj):
 
     return(output1)
 
-
+#post request
 def netto1(brutto):
     headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36",
